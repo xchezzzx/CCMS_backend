fbbe511a1bfb883295fc1dc6739b3b18f07a0cbd
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2023-01-14 23:36:04 +0200

    parsers for entities

--- ASPNETCore/DataAccess/Models/DBModels/ExerciseCategory.cs
+++ ASPNETCore/DataAccess/Models/DBModels/ExerciseCategory.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 
 namespace ASPNETCore.DataAccess.Models.DBModels
 {
-    public partial class ExerciseCategory : ICRUDEntity
-    {
-        public ExerciseCategory()
-        {
-            Exercises = new HashSet<Exercise>();
-        }
+	public partial class ExerciseCategory : ICRUDEntity
+	{
+		public ExerciseCategory()
+		{
+			Exercises = new HashSet<Exercise>();
+		}
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreateDate { get; set; }
-        public int CreateUserId { get; set; }
-        public DateTime UpdateDate { get; set; }
-        public int UpdateUserId { get; set; }
-        public int StatusId { get; set; }
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime CreateDate { get; set; }
+		public int CreateUserId { get; set; }
+		public DateTime UpdateDate { get; set; }
+		public int UpdateUserId { get; set; }
+		public int StatusId { get; set; }
 
-        public virtual User CreateUser { get; set; }
-        public virtual Status Status { get; set; }
-        public virtual User UpdateUser { get; set; }
-        public virtual ICollection<Exercise> Exercises { get; set; }
-    }
+		public virtual User CreateUser { get; set; }
+		public virtual Status Status { get; set; }
+		public virtual User UpdateUser { get; set; }
+		public virtual ICollection<Exercise> Exercises { get; set; }
+	}
 }
--- ASPNETCore/Helpers/ToDTModelsParsers.cs
+++ ASPNETCore/Helpers/ToDTModelsParsers.cs
@@ -25,9 +25,125 @@ namespace ASPNETCore.Helpers
                 CreateUserId = competition.CreateUserId,
                 UpdateDate = competition.UpdateDate,
                 UpdateUserId = competition.UpdateUserId,
-                Status = ((EntityStatuses)competition.StatusId).ToString()
+                Status = ((EntityStatuses)competition.StatusId).ToString(),
+				StatusId = (EntityStatuses)competition.StatusId
 			};
         }
-    }
+
+        public static UserDT DTUserParser(User user)
+        {
+            return new UserDT()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,  
+                LastName= user.LastName,
+				Email = user.Email,
+                Password = user.Password,
+                PointsSummary = user.PointsSummary,
+                Role = ((Roles)user.RoleId).ToString(),
+                RoleId = (Roles)user.RoleId,
+
+				CreateDate = user.CreateDate,
+				CreateUserId = user.CreateUserId,
+				UpdateDate = user.UpdateDate,
+				UpdateUserId = user.UpdateUserId,
+				Status = ((EntityStatuses)user.StatusId).ToString(),
+                StatusId = (EntityStatuses)user.StatusId
+			};
+        }
+
+		public static TeamDT DTTeamParser(Team team)
+		{
+			return new TeamDT()
+			{
+				Id = team.Id,
+				Name = team.Name,
+				Icon = team.Icon,
+                SumPoints = team.SumPoints,
+
+				CreateDate = team.CreateDate,
+				CreateUserId = team.CreateUserId,
+				UpdateDate = team.UpdateDate,
+				UpdateUserId = team.UpdateUserId,
+				Status = ((EntityStatuses)team.StatusId).ToString(),
+				StatusId = (EntityStatuses)team.StatusId
+			};
+		}
+
+		public static ExerciseDT DTExerciseParser(Exercise exercise)
+		{
+			return new ExerciseDT()
+			{
+				Id = exercise.Id,
+				Name = exercise.Name,
+				CategoryId = exercise.CategoryId,
+				LangId = exercise.LangId,
+				PlatformId = exercise.PlatformId,
+				Timeframe = exercise.Timeframe,
+				Points = exercise.Points,
+				Fine = exercise.Fine,
+				IfHasBonus = exercise.IfHasBonus,
+				BonusContent = exercise.BonusContent,
+				BonusTimeframe = exercise.BonusTimeframe,
+				BonusPoints = exercise.BonusPoints,
+
+				CreateDate = exercise.CreateDate,
+				CreateUserId = exercise.CreateUserId,
+				UpdateDate = exercise.UpdateDate,
+				UpdateUserId = exercise.UpdateUserId,
+				Status = ((EntityStatuses)exercise.StatusId).ToString(),
+				StatusId = (EntityStatuses)exercise.StatusId
+			};
+		}
+
+		public static ExerciseCategoryDT DTExerciseCategoryParser(ExerciseCategoryDT category)
+		{
+			return new ExerciseCategoryDT()
+			{
+				Id = category.Id,
+				Name = category.Name,
+
+				CreateDate = category.CreateDate,
+				CreateUserId = category.CreateUserId,
+				UpdateDate = category.UpdateDate,
+				UpdateUserId = category.UpdateUserId,
+				Status = ((EntityStatuses)category.StatusId).ToString(),
+				StatusId = (EntityStatuses)category.StatusId
+			};
+		}
+
+		public static ExerciseLangDT DTExerciseLanguageParser(ExerciseLangDT lang)
+		{
+			return new ExerciseLangDT()
+			{
+				Id = lang.Id,
+				Name = lang.Name,
+
+				CreateDate = lang.CreateDate,
+				CreateUserId = lang.CreateUserId,
+				UpdateDate = lang.UpdateDate,
+				UpdateUserId = lang.UpdateUserId,
+				Status = ((EntityStatuses)lang.StatusId).ToString(),
+				StatusId = (EntityStatuses)lang.StatusId
+			};
+		}
+
+		public static ExercisePlatformDT DTExercisePlatformParser(ExercisePlatformDT platform)
+		{
+			return new ExercisePlatformDT()
+			{
+				Id = platform.Id,
+				Name = platform.Name,
+
+				CreateDate = platform.CreateDate,
+				CreateUserId = platform.CreateUserId,
+				UpdateDate = platform.UpdateDate,
+				UpdateUserId = platform.UpdateUserId,
+				Status = ((EntityStatuses)platform.StatusId).ToString(),
+				StatusId = (EntityStatuses)platform.StatusId
+			};
+		}
+
+	}
 }
 
